0965d26ba86c5060b569694f53457cbcbc673c0e
Iorgen Wildrik <w.drik@outlook.com>
2020-03-31 22:32:19 -0300

    backend successfully completed!

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -25,7 +27,12 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.post('/sessions', SessionController.create);
+
+routes.get('/profile', ProfileController.index);
+
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
