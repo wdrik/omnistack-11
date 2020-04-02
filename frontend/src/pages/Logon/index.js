fd3f9421fe404bb2114794561880cf1b4b083fe9
Iorgen Wildrik <w.drik@outlook.com>
2020-04-02 00:32:18 -0300

    Completed CSS Logon page!

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -10,6 +11,21 @@ export default function Logon() {
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </a>
+        </form>
       </section>
 
       <img src={heroesImg} alt="Heroes" />
